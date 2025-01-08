df128985012f012c6f2be46412a2c9787eff8aae
gxstar <gengxing123@qq.com>
2025-01-08 16:56:45 +0800

    添加token验证

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import mainRoutes from './routes/mainRoutes'
 import equipmentRoutes from './routes/equipmentRoutes'
 import userRoutes from './routes/userRoutes'
 import { useUserStore } from '@/stores/userStore'
+import axios from 'axios'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,13 +14,36 @@ const router = createRouter({
   ]
 })
 // 添加全局路由守卫
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   const userStore = useUserStore()
-
+  const userData = JSON.parse(localStorage.getItem('user'));
+  const token = userData?.token ?? null;
   // 检查是否需要登录
   if (to.meta.requiresAuth && !userStore.isLoggedIn()) {
     next('/login') // 跳转到登录页面
     return
+  }
+
+  // 如果已登录，验证token有效性
+  if (userStore.isLoggedIn()) {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/users/verify-token', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!response.data.valid) {
+        userStore.logout()
+        next('/login')
+        return
+      }
+    } catch (error) {
+      console.error('Token验证失败:', error)
+      userStore.logout()
+      next('/login')
+      return
+    }
   }
 
   // 检查是否需要管理员权限
